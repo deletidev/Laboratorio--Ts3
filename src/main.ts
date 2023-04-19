bc1bd764184a01c6e9aad88bff33ac334edfd352
LeticiadelaOsa <deletidev@gmail.com>
2023-04-19 12:34:30 +0200

    simplificando

--- src/main.ts
+++ src/main.ts
@@ -5,171 +5,52 @@ const next = document.getElementById('next');
 const prev = document.getElementById('prev');
 const changeTurn = document.getElementById('turn-change');
 const turn = document.getElementById('turn');
+let count: number;
 
-const desactivateBtn = (btn: HTMLButtonElement): void => {
-  btn.disabled = true;
-};
-const activateBtn = (btn: HTMLButtonElement): void => {
-  btn.disabled = false;
-};
-
-if (
-  turn !== null &&
-  turn !== undefined &&
-  next instanceof HTMLButtonElement &&
-  next !== null &&
-  next !== undefined &&
-  prev instanceof HTMLButtonElement &&
-  prev !== null &&
-  prev !== undefined &&
-  reset !== null &&
-  reset !== undefined &&
-  changeTurn instanceof HTMLInputElement &&
-  changeTurn !== null &&
-  changeTurn !== undefined
-) {
-  let count = parseInt(changeTurn.value);
-
-  const prevFc = (): void => {
-    activateBtn(next);
-    count--;
-    turn.textContent = count.toString().padStart(2, '0');
-    changeTurn.value = count.toString();
-    if (count === 1) {
-      desactivateBtn(prev);
-    }
-  };
-
-  const nextFc = (): void => {
-    activateBtn(prev);
-    count++;
-    turn.textContent = count.toString().padStart(2, '0');
-    changeTurn.value = count.toString();
-    if (count === 99) {
-      desactivateBtn(next);
-    }
-  };
-
-  const resetFc = (): void => {
-    turn.textContent = '01';
-    changeTurn.value = '1';
-    count = 1;
-    desactivateBtn(prev);
-  };
-
-  const changeFc = (): void => {
-    activateBtn(prev);
-    activateBtn(next);
-    let number = changeTurn.value;
-    turn.textContent = number.toString().padStart(2, '0');
-    count = parseInt(number);
-    if (parseInt(number) >= 99) {
-      number = '99';
-      count = 99;
-      turn.textContent = '99';
-    }
-    if (parseInt(number) <= 1 || number === '') {
-      number = '1';
-      count = 1;
-      turn.textContent = '01';
-    }
-    if (count <= 1) {
-      desactivateBtn(prev);
-    }
-    if (count >= 99) {
-      desactivateBtn(next);
-    }
-  };
-  console.log('aqui');
-  next.addEventListener('click', nextFc);
-  prev.addEventListener('click', prevFc);
-  reset.addEventListener('click', resetFc);
-  changeTurn.addEventListener('change', changeFc);
+if (turn && turn.textContent) {
+  count = parseInt(turn.textContent);
 }
 
-// const desactivateBtn = (btn: any): void => {
-//   if (btn instanceof HTMLButtonElement) {
-//     btn.disabled = true;
-//   }
-// };
-// const activateBtn = (btn: any): void => {
-//   if (btn instanceof HTMLButtonElement) {
-//     btn.disabled = false;
-//   }
-// };
-// if (reset !== null && reset !== undefined) {
-//   reset.addEventListener('click', () => {
-//     if (
-//       turn !== null &&
-//       turn !== undefined &&
-//       changeTurn instanceof HTMLInputElement &&
-//       changeTurn !== null &&
-//       changeTurn !== undefined
-//     ) {
-//       turn.textContent = '01';
-//       changeTurn.value = '1';
-//       count = 1;
-//       desactivateBtn(prev);
-//     }
-//   });
-// }
+const prevFc = (): void => {
+  if (turn) {
+    count--;
+    turn.textContent = count.toString().padStart(2, '0');
+  }
+};
 
-// if (next !== null && next !== undefined) {
-//   next.addEventListener('click', () => {
-//     activateBtn(prev);
-//     count++;
-//     if (turn !== null && turn !== undefined) {
-//       turn.textContent = count.toString().padStart(2, '0');
-//       changeTurn.value = count.toString();
-//     }
-//     if (count === 99) {
-//       desactivateBtn(next);
-//     }
-//   });
-// }
+const nextFc = (): void => {
+  if (turn) {
+    count++;
+    turn.textContent = count.toString().padStart(2, '0');
+  }
+};
 
-// if (prev !== null && prev !== undefined) {
-//   prev.addEventListener('click', () => {
-//     activateBtn(next);
-//     count--;
-//     if (turn !== null && turn !== undefined) {
-//       turn.textContent = count.toString().padStart(2, '0');
-//       changeTurn.value = count.toString();
-//     }
-//     if (count === 1) {
-//       desactivateBtn(prev);
-//     }
-//   });
-// }
+const resetFc = (): void => {
+  if (turn && changeTurn instanceof HTMLInputElement) {
+    turn.textContent = '01';
+    changeTurn.value = '';
+    count = 1;
+  }
+};
 
-// if (
-//   changeTurn instanceof HTMLInputElement &&
-//   changeTurn !== null &&
-//   changeTurn !== undefined
-// ) {
-//   changeTurn.addEventListener('change', () => {
-//     activateBtn(prev);
-//     activateBtn(next);
-//     let number = changeTurn.value;
-//     if (turn !== null && turn !== undefined) {
-//       turn.textContent = number.toString().padStart(2, '0');
-//       count = parseInt(number);
-//       if (parseInt(number) >= 99) {
-//         number = '99';
-//         count = 99;
-//         turn.textContent = '99';
-//       }
-//       if (parseInt(number) <= 1 || number === '') {
-//         number = '1';
-//         count = 1;
-//         turn.textContent = '01';
-//       }
-//     }
-//     if (count <= 1) {
-//       desactivateBtn(prev);
-//     }
-//     if (count >= 99) {
-//       desactivateBtn(next);
-//     }
-//   });
-// }
+const changeFc = (): void => {
+  if (turn && changeTurn instanceof HTMLInputElement) {
+    count = parseInt(changeTurn.value);
+    turn.textContent = count.toString();
+  }
+};
+
+if (next) {
+  next.addEventListener('click', nextFc);
+}
+
+if (prev) {
+  prev.addEventListener('click', prevFc);
+}
+
+if (reset) {
+  reset.addEventListener('click', resetFc);
+}
+if (changeTurn) {
+  changeTurn.addEventListener('change', changeFc);
+}
